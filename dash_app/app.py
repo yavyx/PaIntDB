ac94bec2f556f24e6fb9cf0d44d0105547d4de7f
yavyx <javiercastilloar@gmail.com>
2019-09-06 15:47:24 -0700

    added upload functionality

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,22 +1,31 @@
+import base64
+import io
+
 import dash
+from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
+import pandas as pd
 
+import bio_networks.network_generator as ng
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H1(children='PaIntDB'),
+    html.H1('PaIntDB'),
 
-    dcc.Upload(html.Button('Upload Gene List')),
+    dcc.Upload(html.Button('Upload Gene List'), id='gene-list-upload'),
+
+    html.Div(id='output-data-upload'),
 
     html.Hr(),
 
-
-    html.Div(children='Select the strain:'),
+    html.Div('Select the strain:'),
     dcc.RadioItems(
+        id='strain',
         options=[
             {'label': 'PAO1', 'value': 'PAO1'},
             {'label': 'PA14', 'value': 'PA14'},
@@ -24,8 +33,9 @@ app.layout = html.Div([
         value='PAO1'
     ),
 
-    html.Div(children='Select the network order:'),
+    html.Div('Select the network order:'),
     dcc.RadioItems(
+        id='order',
         options=[
             {'label': 'Zero-order', 'value': 0},
             {'label': 'First-order', 'value': 1},
@@ -33,8 +43,9 @@ app.layout = html.Div([
         value=0
     ),
 
-    html.Div(children='Select the interaction detection method:'),
+    html.Div('Select the interaction detection method:'),
     dcc.RadioItems(
+        id='detection-method',
         options=[
             {'label': 'All', 'value': 'all'},
             {'label': 'Experimental', 'value': 'experimental'},
@@ -44,15 +55,44 @@ app.layout = html.Div([
         value='all'
     ),
 
-    html.Div(children='Include metabolites?'),
+    html.Div('Include metabolites?'),
     dcc.RadioItems(
+        id='metabolites',
         options=[
             {'label': 'Yes', 'value': 'True'},
             {'label': 'No', 'value': 'False'},
         ],
         value='False'
     ),
+
+    html.Button('Make network'),
+
+    html.Hr(),
+
+    html.Button('Download network (.graphml)')
 ])
+
+
+def parse_gene_list(contents):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+    return html.Div([
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        )
+    ])
+
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    [Input('gene-list-upload', 'contents')]
+)
+def update_output(contents):
+    children = parse_gene_list(contents)
+    return children
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
